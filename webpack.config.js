64b7480daa55f0da29191926e0f7ed4c2a1fe61b
Tomas <tomas.peterk@gmail.com>
2019-03-10 08:59:11 +0100

    Prepare docs/ for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 module.exports = {
 	mode: "development",
 	entry: {
 		navigator: "./src/navigator.js",
 		editor: "./src/editor.js",
 		main: "./src/index.js"
+	},
+	output: {
+		path: path.resolve(__dirname, "docs")
 	}
 }
