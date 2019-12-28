a099901947edff9ca4e20c5639a0b3fd9d927877
Tomas Peterka <prestizni@gmail.com>
2019-12-28 18:49:28 +0100

    Add webserver and webdav server for testing

---
+++ serve.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const webdav = require('webdav-server').v2;
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + "/" + "index.html" );
+})
+
+// Mount the WebDAVServer instance
+const webdav_server = new webdav.WebDAVServer();
+webdav_server.setFileSystem('', new webdav.PhysicalFileSystem(__dirname + "/dav-root"));
+app.use(webdav.extensions.express('/dav', webdav_server));
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Web server listening at http://%s:%s", host, port)
+   console.log("WebDav mount point at http://%s:%s/dav", host, port)
+})
