061525b73c7c4f03ca32d2bcfcd04a977be35567
Tomas Peterka <tomas@peterka.me>
2020-04-21 23:02:56 +0200

    Navigator always stats its path and decides inteligently

--- src/navigator.js
+++ src/navigator.js
@@ -3,6 +3,22 @@
 import { createClient as webdavClient } from "webdav";
 import { LitElement, html, css } from "lit-element";
 
+/* WebDAV file object examples
+{basename: "a.md"
+etag: "679e6de74f05cc755edc4a5ae9195bc4"
+filename: "/dir1/a.md"
+lastmod: "Mon, 20 Apr 2020 19:30:42 GMT"
+mime: "text/markdown"
+size: 0
+type: "file"}
+----------------------------------------
+{basename: "dir11"
+etag: "a5138e5406c0c8b3521486dc73b950b4"
+filename: "/dir1/dir11"
+lastmod: "Mon, 20 Apr 2020 19:30:46 GMT"
+size: 0
+type: "directory"}
+*/ 
 
 function matchHashOr(expression, defaultValue) {
 	const matched = window.location.hash.match(expression);
@@ -30,8 +46,8 @@ function stop(event) {
 }
 
 function pathJoin(/*paths...*/) {
-	let path = "";
-	for (var i = 0; i < arguments.length; i++) {
+	let path = arguments[0];
+	for (var i = 1; i < arguments.length; i++) {
 		if(path.endsWith("/")) path = path + arguments[i];
 		else path = path + "/" + arguments[i];
 	}
@@ -81,7 +97,7 @@ class Navigator extends LitElement {
 			.then(stat => {
 				storeValue("navigator.username", this.username);
 				storeValue("navigator.url", this.url);
-				return this.navigate({filename: "/", basename: "", type: "directory"});
+				return this.navigate("/");
 			})
 			.then(() => {
 				this.connected = true;
@@ -108,30 +124,35 @@ class Navigator extends LitElement {
 		return this.client.putFileContents(filename, content);
 	}
 
-	navigate(item, event) {
+	navigate(path, event) {
 		stop(event);
-		if(item.type == "directory") {
-			const parts = item.filename.split("/");
-			this.crumbs = [{filename: "/", basename: window.location.hostname, type: "directory"}];
-			for(let i = 1; i < parts.length && parts[i].filename != ""; i++) {
-				this.crumbs.push({filename: this.crumbs[i-1].filename + "/" + parts[i], basename: parts[i], type: "directory"});
-			}
-			return this.client.getDirectoryContents(item.filename).then(
-				content => {
-					this.content = content;
-					this.path = item.filename;
-				});
-
-		} else if(item.type == "file") {
-			return this.client.stat(item.filename).then(
-				stats => {
+		return this.client.stat(path).then(
+			stats => {
+				if(stats.type == "file") {
 					if(!stats.mime.startsWith("text")) throw new Error("Can open only text files");
 					if(stats.size > 512000) throw new Error("Cannot open too big files (512kB is the limit)");
-					return this.client.getFileContents(item.filename, {"format": "text"})
-				}).then(
-				content => this.open(item.filename, content)
-				);
-		}
+					return this.client.getFileContents(path, {"format": "text"}).then(
+						content => this.open(path, content)
+					);
+				}
+				else if(stats.type == "directory") {
+					const parts = path.split("/");
+					this.crumbs = [{filename: "/", basename: window.location.hostname}];
+					for(let i = 1; i < parts.length && parts[i].filename != ""; i++) {
+						this.crumbs.push({filename: pathJoin(this.crumbs[i-1].filename, parts[i]), basename: parts[i]});
+					}
+					return this.client.getDirectoryContents(path).then(
+						content => {
+							this.content = content;
+							if(this.path == path) {
+								return this.requestUpdate()
+							}
+							this.path = path;
+							return this.updateComplete;
+						});
+				}
+				else throw new Error(`Does $path really exist?`);
+			});
 	}
 
 	newContentHandler(event) {
@@ -147,15 +168,10 @@ class Navigator extends LitElement {
 		const filePath = pathJoin(this.path, name);
 		const isDir = (name.indexOf(".") < 0);
 
-		return isDir?
-			this.client.createDirectory(filePath).then(
-				() => this.navigate({filename: filePath, "type": "directory"})
-			):
-			this.save(filePath, "").then(
-				() => this.navigate({filename: this.path, "type": "directory"})
-				).then(
-				() => this.navigate({filename: filePath, "type": "file"})
-			);
+		return (isDir?
+			this.client.createDirectory(filePath):
+			this.save(filePath, "").then(() => this.open(filePath, ""))
+		).then(() => this.navigate(this.path)); // a.k.a. refresh
 	}
 
 	static get styles() {
@@ -181,13 +197,13 @@ class Navigator extends LitElement {
 		return html`
 			${this.connected?
 				html`
-					${this.crumbs.map(item => html`/ <a href="" @click="${(e) => this.navigate(item, e)}">${item.basename}</a>`)}
+					${this.crumbs.map(item => html`/ <a href="" @click="${this.navigate.bind(this, item.filename)}">${item.basename}</a>`)}
 					<ul>
 						${(this.content.length == 0)?
 							html`<li>&lt;empty&gt;</li>`:
 							html`${this.content.map(item => html`
 								<li>
-									<a href="" @click="${(e) => this.navigate(item, e)}">${item.basename}</a>
+									<a href="" @click="${(e) => this.navigate(item.filename, e)}">${item.basename}</a>
 								</li>`)}`
 						}
 						${this.connected?
